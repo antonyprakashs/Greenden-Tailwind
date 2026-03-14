3082d99607225464a79e95f9f7833d9b138f6542
antonyprakashs <prakashantony005@gmail.com>
2026-03-14 12:17:15 +0530

    Updated search

--- product.js
+++ product.js
@@ -12,3 +12,26 @@ closenav.addEventListener("click", function()
 {
     sidenav.style.right = "-50%"
 })
+
+// Product Search
+
+let product = document.getElementById("product")
+let search = document.getElementById("search")
+let productList = product.querySelectorAll("div")
+
+search.addEventListener("keyup", function(){
+    let enteredValue = event.target.value.toUpperCase()
+
+    for (let count = 0; count < productList.length; count = count+1) {
+
+        let productname = productList[count].querySelector("h1").textContent
+        
+        if (productname.toUpperCase().indexOf(enteredValue)<0) {
+            productList[count].style.display = "none"
+        }
+        
+        else {
+            productList[count].style.display = "block"
+        }
+    }
+})
